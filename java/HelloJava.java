078668183c02f1fc5d8495cb88820a601af15914
Nagesh Joshi <nagesh.joshi@gmail.com>
2020-08-02 17:49:32 +0530

    Update HelloJava.java
    Nagesh Joshi Changed

--- java/HelloJava.java
+++ java/HelloJava.java
@@ -5,7 +5,7 @@ public static void main(String args[]){
 
 for(int i=0;i<10;i++){
 	
-	System.out.println("Joshi JI Good Evening :"+i );
+	System.out.println("Joshi Family Good Evening :"+i );
 }	
 	
 }
